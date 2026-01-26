8b02e50225e0666b8cf17634af79ffa5c085cc79
Michael Heudorn <michael.heudorn@gmail.com>
2026-01-26 17:26:43 +0100

    Added logging

--- src/mariadb_trace/mariadb_trace.c
+++ src/mariadb_trace/mariadb_trace.c
@@ -6,9 +6,11 @@
 #include <signal.h>
 #include <stdio.h>
 #include <sys/resource.h>
+#include <time.h>
 #include <unistd.h>
 
 static int stop = 0;
+FILE *log_file = NULL;
 
 void sig_handler(int signo) { stop = 1; }
 
@@ -16,6 +18,50 @@ int is_dump_tool(const char *query) {
   if (strstr(query, "/*!") && strstr(query, "SQL_NO_CACHE"))
     return 1;
   return 0;
+}
+
+void get_timestamp(char *buffer, size_t size) {
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+  strftime(buffer, size, "%Y-%m-%d %H:%M:%S", t);
+}
+
+void json_escape_string(char *dest, size_t dest_size, const char *src) {
+  size_t d = 0;
+  for (size_t s = 0; src[s] != '\0'; s++) {
+    if (d >= dest_size - 3)
+      break;
+
+    char c = src[s];
+
+    switch (c) {
+    case '\"': // Anführungszeichen
+      dest[d++] = '\\';
+      dest[d++] = '\"';
+      break;
+    case '\\': // Backslash
+      dest[d++] = '\\';
+      break;
+    case '\n': // Zeilenumbruch
+      dest[d++] = '\\';
+      dest[d++] = 'n';
+      break;
+    case '\r': // Carriage Return
+      dest[d++] = '\\';
+      dest[d++] = 'r';
+      break;
+    case '\t': // Tab
+      dest[d++] = '\\';
+      dest[d++] = 't';
+      break;
+    default:
+      if ((unsigned char)c >= 32) {
+        dest[d++] = c;
+      }
+      break;
+    }
+  }
+  dest[d] = '\0';
 }
 
 int handle_event(void *ctx, void *data, size_t data_sz) {
@@ -30,16 +76,24 @@ int handle_event(void *ctx, void *data, size_t data_sz) {
     is_suspicious = is_suspicious || is_dump_tool(e->query);
   }
 
-  if (is_suspicious) {
-    printf("[%s] PID: %d | Time: %.2f ms | Bytes: %llu | Query: %s\n", "ALERT",
-           e->mariadb_pid, duration_ms, e->bytes_sent, e->query);
+  if (!ALERT_ENABLE_INFO_LOGGING && !is_suspicious) {
+    return 0;
   }
 
-  if (ALERT_ENABLE_INFO_LOGGING && !is_suspicious) {
-    printf("[%s] PID: %d | Time: %.2f ms | Bytes: %llu | Query: %s\n", "INFO ",
-           e->mariadb_pid, duration_ms, e->bytes_sent, e->query);
-  }
+  char time_str[32];
+  char query_escaped[MAX_SQL_LEN * 2 + 1];
 
+  json_escape_string(query_escaped, sizeof(query_escaped), e->query);
+  get_timestamp(time_str, sizeof(time_str));
+
+  fprintf(log_file,
+          "{\"timestamp\": \"%s\", \"level\": \"%s\", \"pid\": %d,"
+          "\"bytes\": %llu, \"duration_ms\": %.2f, "
+          "\"query\": \"%s\"}\n",
+          time_str, is_suspicious ? "ALERT" : "INFO", e->mariadb_pid,
+          e->bytes_sent, duration_ms, query_escaped);
+
+  fflush(log_file);
   return 0;
 }
 
@@ -53,6 +107,13 @@ int main(int argc, char **argv) {
   // Set libbpf print function
   signal(SIGINT, sig_handler);
   signal(SIGTERM, sig_handler);
+
+  // Initalize Log File
+  log_file = fopen("/var/log/mariadb_dlp.log", "a");
+  if (!log_file) {
+    fprintf(stderr, "Failed to open log_file!\n");
+    return 1;
+  }
 
   // Open BPF program, returns the skeleton object
   skel = mariadb_trace_bpf__open();
@@ -99,6 +160,7 @@ int main(int argc, char **argv) {
 cleanup:
   // Destroy BPF program
   ring_buffer__free(rb);
+  fclose(log_file);
   mariadb_trace_bpf__destroy(skel);
 
   return err < 0 ? -err : 0;
